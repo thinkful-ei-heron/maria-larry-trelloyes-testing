8178b421d762338c9f17a8a3e0fbc3b1ed6fa2fd
mal3905 <52715674+mal3905@users.noreply.github.com>
2019-10-08 14:53:08 -0400

    Update List.test.js

--- src/List.test.js
+++ src/List.test.js
@@ -2,15 +2,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './List';
+import renderer from 'react-test-renderer';
 
-const allCard = [
-  { id: 'a', title: 'First card', content: 'lorem ipsum' },
-  { id: 'b', title: 'Second card', content: 'lorem ipsum' },
-  { id: 'c', title: 'Third card', content: 'lorem ipsum' }
-]
+describe('List component', () => {
+  
+  const allCard = [
+    { id: 'a', title: 'First card', content: 'lorem ipsum' },
+    { id: 'b', title: 'Second card', content: 'lorem ipsum' },
+    { id: 'c', title: 'Third card', content: 'lorem ipsum' }
+  ]
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<List cards={allCard} />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List cards={allCard} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+    .create(<List cards={allCard}/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
+
 });
