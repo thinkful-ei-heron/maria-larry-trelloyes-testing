6799fdab08829ec4559c9e3c0636649b79b996fd
mal3905 <52715674+mal3905@users.noreply.github.com>
2019-10-08 14:52:22 -0400

    Update Card.test.js

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,11 +1,20 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card';
+import renderer from 'react-test-renderer';
 
+describe('Messages component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card name="Card"/>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+  });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
+
